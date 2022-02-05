f59a8a117bfa7dc7ba895815e22052bc8f2068b4
kaliaaaaan <99092287+kaliaaaaan@users.noreply.github.com>
2022-02-05 21:41:53 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -11,15 +11,15 @@ $email = $_POST['user_email'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'nikolay.prons@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'FwbzO3Jx'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('kalian48@yandex,ru');     // Кому будет уходить письмо 
+$mail->setFrom('nikolay.prons@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('kalian48@yandex.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
